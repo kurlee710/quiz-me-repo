350566c3f69414d64a3b323e41453eb8a6e11a91
Aditi Rai <aditi45@gmail.com>
2024-09-12 12:05:29 -0400

    created timer function and called it in addEventListener function

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,24 @@
 // start quiz button to kick off quiz function
 const startBtn = document.getElementById("start-quiz");
+const timerEl = document.querySelector(".timer");
+let timeLeft = 30;
+
+function startTimer() {
+  let timerInterval = setInterval(function () {
+    timeLeft--;
+    timerEl.textContent = timeLeft + " seconds remaining";
+    if (timeLeft === 0) {
+      clearInterval(timerInterval);
+      alert("Time's up!");
+    }
+  }, 1000);
+}
 
 // function startQuiz() {
 startBtn.addEventListener("click", function () {
   console.log("start quiz button clicked");
-  renderQuestion();
   startTimer();
+  renderQuestion();
 });
 // }
 
@@ -17,6 +30,8 @@ for (let i = 0; i < questions.choices.length; i++) {
 // if-else statement that compares the user answer to correct choice and adds to user score
 if (userAnswer === questions.correctChoice) {
   userScore++;
+} else {
+  userScore--;
 }
 // store each question score in local storage - json.stringify - part of if-else statement
 localStorage.setItem("userScore", JSON.stringify(userScore));

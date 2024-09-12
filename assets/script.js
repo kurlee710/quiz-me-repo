cf9ce1970ecfb7fb071c5f10240097af510149b7
lekshmisree@gmail.com <lekshmisree89@gmail.com>
2024-09-12 10:57:25 -0400

    array of objects added in js file

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,23 @@
-// create an empty array that stores correct answers
-const quizAnswers = [];
+// create an array of objects  that stores correct answers
+const quizData = [
+{
+question: 'what does HTML stand for?',
+choices:['1.Hyper Text Markup Language','2.Home Tool Markup Language','3.Hyperlinks and Text Markup Language'],
+CorrectAnswer :'Hyper Text Markup Language',
+},
+
+{
+    question:'Which HTML attribute is used to define inline styles?',
+    choices: ['1.styles','2.style','3.class'],
+    CorrectAnswer :'1.styles',
+    
+},
+{
+    question:'In a CSS document, how do you select all p elements inside a div element?',
+    choices:['1.div + p','2. div p','3.div.p'],
+    CorrectAnswer :'2.div p',
+      
+}
+
+]
+

a0dd52b780d0507ca89b2a11eb27aefd89d5e656
lekshmisree@gmail.com <lekshmisree89@gmail.com>
2024-09-12 13:47:53 -0400

    functions propely placed

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,48 @@
 // DEPENDENCIES ------------------------------------------------------------
 // start quiz button to kick off quiz function
 const startBtn = document.getElementById("start-quiz");
+const questionData = quizData[currentQuestionIndex];
+const quizContainer = document.getElementsByClassName('card');
+const feedbackEl = document.getElementById('feedback');
+const scoreBoardEl = document.getElementById('score-board');
+
+ 
+// create an array of objects  that stores correct answers
+const quizData = [
+    {
+    question: 'what does HTML stand for?',
+    choices:['1.Hyper Text Markup Language','2.Home Tool Markup Language','3.Hyperlinks and Text Markup Language'],
+    CorrectAnswer :'Hyper Text Markup Language',
+    },
+    
+    {
+        question:'Which HTML attribute is used to define inline styles?',
+        choices: ['1.styles','2.style','3.class'],
+        CorrectAnswer :'1.styles',
+        
+    },
+    {
+        question:'In a CSS document, how do you select all p elements inside a div element?',
+        choices:['1.div + p','2. div p','3.div.p'],
+        CorrectAnswer :'2.div p',
+          
+    }
+    
+    ]
+    
+
 const timerEl = document.querySelector(".timer");
 
 // DATA -------------------------------------------------------------------
 let timeLeft = 30;
 let userScore = 0;
+let currentQuestionIndex = 0;
+
+
+
+// creating event listeners for loading a quiz and handling the next question.
+document.addEventListener('DOMContentLoaded', loadQuiz);
+document.getElementsById('start-quiz').addEventListener('click', renderQuestion);
 
 // FUNCTIONS --------------------------------------------------------------
 function startTimer() {
@@ -49,43 +86,12 @@ if (userAnswer === quizData.correctChoice) {
 }
 // store each question score in local storage - json.stringify - part of if-else statement
 localStorage.setItem("userScore", JSON.stringify(userScore));
-//DATA
-const questionData = quizData[currentQuestionIndex];
-const quizContainer = document.getElementsByClassName('card');
-const feedbackEl = document.getElementById('feedback');
-const scoreBoardEl = document.getElementById('score-board');
 
 
 
-// create an array of objects  that stores correct answers
-const quizData = [
-{
-question: 'what does HTML stand for?',
-choices:['1.Hyper Text Markup Language','2.Home Tool Markup Language','3.Hyperlinks and Text Markup Language'],
-CorrectAnswer :'Hyper Text Markup Language',
-},
-
-{
-    question:'Which HTML attribute is used to define inline styles?',
-    choices: ['1.styles','2.style','3.class'],
-    CorrectAnswer :'1.styles',
-    
-},
-{
-    question:'In a CSS document, how do you select all p elements inside a div element?',
-    choices:['1.div + p','2. div p','3.div.p'],
-    CorrectAnswer :'2.div p',
-      
-}
-
-]
-
-let currentQuestionIndex = 0;
 
 
-// creating event listeners for loading a quiz and handling the next question.
-document.addEventListener('DOMContentLoaded', loadQuiz);
-document.getElementsById('start-quiz').addEventListener('click', renderQuestion);
+
 
   // Function to render the current question
   // when the start quiz button os clicked

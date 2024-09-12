4c1495997c83ebf1e05080504480059d43f8ef22
Aditi Rai <aditi45@gmail.com>
2024-09-12 11:19:38 -0400

    console.log to check that start quiz button functionality is working

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,18 @@
 // start quiz button to kick off quiz function
 const startBtn = document.getElementById("start-quiz");
 
-function startQuiz() {
-  GamepadButton.addEventListener("click", function () {
-    console.log("start quiz button clicked");
-  });
-}
+// function startQuiz() {
+startBtn.addEventListener("click", function () {
+  console.log("start quiz button clicked");
+  
+});
+// }
 
 // counter for user score
-userScore = 0;
-// if-else statement that compares the user answer to correct choice and adds to user score
-if (userAnswer === correctChoice) {
-  userScore++;
-}
-// store each question score in local storage - json.stringify - part of if-else statement
-localStorage.setItem("userScore", JSON.stringify(userScore));
+// userScore = 0;
+// // if-else statement that compares the user answer to correct choice and adds to user score
+// if (userAnswer === correctChoice) {
+//   userScore++;
+// }
+// // store each question score in local storage - json.stringify - part of if-else statement
+// localStorage.setItem("userScore", JSON.stringify(userScore));

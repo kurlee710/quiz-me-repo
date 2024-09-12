4224462167335d0f0fa2f23366f7a45d7253766d
Aditi Rai <aditi45@gmail.com>
2024-09-12 11:34:34 -0400

    defined userAnswer variable

--- assets/script.js
+++ assets/script.js
@@ -4,15 +4,19 @@ const startBtn = document.getElementById("start-quiz");
 // function startQuiz() {
 startBtn.addEventListener("click", function () {
   console.log("start quiz button clicked");
-  
+  renderQuestion();
+  startTimer();
 });
 // }
 
 // counter for user score
-// userScore = 0;
-// // if-else statement that compares the user answer to correct choice and adds to user score
-// if (userAnswer === correctChoice) {
-//   userScore++;
-// }
-// // store each question score in local storage - json.stringify - part of if-else statement
-// localStorage.setItem("userScore", JSON.stringify(userScore));
+userScore = 0;
+for (let i = 0; i < questions.choices.length; i++) {
+  userAnswer = questions.choices[i];
+}
+// if-else statement that compares the user answer to correct choice and adds to user score
+if (userAnswer === questions.correctChoice) {
+  userScore++;
+}
+// store each question score in local storage - json.stringify - part of if-else statement
+localStorage.setItem("userScore", JSON.stringify(userScore));

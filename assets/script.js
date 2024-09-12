03bd20ba3d4c351a86f05100d93fac02e010ba17
Aditi Rai <aditi45@gmail.com>
2024-09-12 11:13:06 -0400

    create start button variable and function to start quiz

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,12 @@
-// start quiz button to kick off quiz
+// start quiz button to kick off quiz function
+const startBtn = document.getElementById("start-quiz");
+
+function startQuiz() {
+  GamepadButton.addEventListener("click", function () {
+    console.log("start quiz button clicked");
+  });
+}
+
 // counter for user score
 userScore = 0;
 // if-else statement that compares the user answer to correct choice and adds to user score

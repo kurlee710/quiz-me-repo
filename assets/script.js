71a78d653c04b2813ac99878c35fc9575073fd3a
Aditi Rai <aditi45@gmail.com>
2024-09-12 19:15:27 -0400

    modify and clean code

--- assets/script.js
+++ assets/script.js
@@ -8,8 +8,8 @@ const timerEl = document.querySelector(".timer");
 
 // DATA -------------------------------------------------------------------
 let timeLeft = 30;
-
 let currentQuestionIndex = 0;
+
 // create an array of objects  that stores correct answers
 let quizData = [
   {
@@ -34,14 +34,6 @@ let quizData = [
     CorrectAnswer: "2.div p",
   },
 ];
-// start quiz button to kick off quiz function
-
-const questionData = quizData[currentQuestionIndex];
-
-// DATA -------------------------------------------------------------------
-let timeLeft = 30;
-let userScore = 0;
-let currentQuestionIndex = 0;
 
 // FUNCTIONS -----------------------------------------------
 
@@ -56,34 +48,6 @@ function startTimer() {
   }, 1000);
 }
 
-// Function to render the current question when the start quiz button is clicked
-//the loadQuestion function dynamically generates
-//the HTML for each question and its corresponding choices.
-//map() fn is used to loop over the choices array
-//and create radio buttons for each choice.
-
-function loadQuiz() {
-  // Clear any previous feedback CONTENT
-  feedbackEl.innerHTML = "";
-  // Get the current question - created a local variable instead of a global variable. resolves the error of 'Cannot access 'quizData' before initialization'
-  let questionData = quizData[currentQuestionIndex];
-
-  quizContainer.innerHTML = `
-        <h2>${questionData.question}</h2>
-        ${questionData.choices
-          .map(
-            (choice, index) => `
-          <div class="form-check">
-            <input type="radio" name="answer" id="choice${index}" value="${choice}" class="form-check-input">
-            <label for="choice${index}" class="form-check-label">${choice}</label>
-          </div>
-        `
-          )
-          .join("")}
-      `;
-}
-// function to handle the next question when submit button is clicked
-
 // function that stores user answers
 function storeUserAnswer() {
   let userAnswer = "";
@@ -95,8 +59,6 @@ function storeUserAnswer() {
   }
   return userAnswer;
 }
-
-// function that stores user score
 
 // USER INTERACTIONS ------------------------------------------------------
 // function to startQuiz
@@ -133,10 +95,17 @@ console.log(quizData);
 
 nextQuestionBtn.addEventListener("click", nextQuestion);
 
+// Function to render the current question when the start quiz button is clicked
+//the loadQuestion function dynamically generates
+//the HTML for each question and its corresponding choices.
+//map() fn is used to loop over the choices array
+//and create radio buttons for each choice.
 // Modify loadQuiz to take an index to load specific question
 function loadQuiz(questionData) {
+  // Clear any previous feedback CONTENT
   feedbackEl.innerHTML = "";
-
+  // Get the current question - created a local variable instead of a global variable. resolves the error of 'Cannot access 'quizData' before initialization'
+  let questionData = quizData[currentQuestionIndex];
   quizContainer.innerHTML = `
     <h2>${questionData.question}</h2>
     ${questionData.choices
@@ -179,7 +148,6 @@ function calculateUserScore() {
   }
   // store each question score in local storage - json.stringify - part of if-else statement
   localStorage.setItem("userScore", JSON.stringify(userScore));
-  localStorage.setItem("userScore", userScore);
 }
 
 // function for rendering scoreboard

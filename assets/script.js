5de1dc270bd82acd1d716550780d5497ffc1853f
Aditi Rai <aditi45@gmail.com>
2024-09-12 19:06:53 -0400

    removed duplicate code

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,11 @@
 // DEPENDENCIES ------------------------------------------------------------
 const startBtn = document.getElementById("start-quiz");
 const submitBtn = document.getElementById("submit-btn");
+const quizContainer = document.getElementById("quiz-container");
+const feedbackEl = document.getElementById("feedback");
+const scoreBoardEl = document.getElementById("score-board");
+const timerEl = document.querySelector(".timer");
+
 // DATA -------------------------------------------------------------------
 let timeLeft = 30;
 
@@ -30,44 +35,13 @@ let quizData = [
   },
 ];
 // start quiz button to kick off quiz function
-const startBtn = document.getElementById("start-quiz");
+
 const questionData = quizData[currentQuestionIndex];
-const quizContainer = document.getElementById("quiz-container");
-const feedbackEl = document.getElementById("feedback");
-const scoreBoardEl = document.getElementById("score-board");
-const timerEl = document.querySelector(".timer");
 
 // DATA -------------------------------------------------------------------
 let timeLeft = 30;
 let userScore = 0;
 let currentQuestionIndex = 0;
-// create an array of objects that stores questions and correct answers
-let quizData = [
-  {
-    question: "what does HTML stand for?",
-    choices: [
-      "1.Hyper Text Markup Language",
-      "2.Home Tool Markup Language",
-      "3.Hyperlinks and Text Markup Language",
-    ],
-    CorrectAnswer: "Hyper Text Markup Language",
-  },
-
-  {
-    question: "Which HTML attribute is used to define inline styles?",
-    choices: ["1.styles", "2.style", "3.class"],
-    CorrectAnswer: "1.styles",
-  },
-  {
-    question:
-      "In a CSS document, how do you select all p elements inside a div element?",
-    choices: ["1.div + p", "2. div p", "3.div.p"],
-    CorrectAnswer: "2.div p",
-  },
-];
-
-// creating event listeners for loading a quiz and handling the next question.
-//document.addEventListener('DOMContentLoaded', loadQuiz);
 
 // FUNCTIONS -----------------------------------------------
 
@@ -139,47 +113,8 @@ submitBtn.addEventListener("click", function () {
   console.log(userAnswer);
 });
 
-// document.addEventListener("DOMContentLoaded", loadQuiz); - do not need this line of code
-//document.addEventListener('DOMContentLoaded', renderQuestion); - do not need this line of code
-
 // INITIALIZATION ---------------------------------------------------------
 storeUserAnswer();
-
-//   // store each question score in local storage - json.stringify - part of if-else statement
-//   localStorage.setItem("userScore", JSON.stringify(userScore));
-  console.log("start quiz button clicked");
-  startTimer();
-  loadQuiz(quizData[0]);
-});
-
-document.addEventListener("DOMContentLoaded", loadQuiz);
-
-// Function to render the current question
-// when the start quiz button os clicked
-
-function loadQuiz() {
-  // Clear any previous feedback CONTENT
-  feedbackEl.innerHTML = "";
-
-  quizContainer.innerHTML = `
-      <h2>${questionData.question}</h2>
-      ${questionData.choices
-        .map(
-          (choice, index) => `
-        <div class="form-check">
-          <input type="radio" name="answer" id="choice${index}" value="${choice}" class="form-check-input">
-          <label for="choice${index}" class="form-check-label">${choice}</label>
-        </div>
-      `
-        )
-        .join("")}
-    `;
-}
-
-
-
-// INITIALIZATION ---------------------------------------------------------
-
 
 // Create a Next Question button
 const nextQuestionBtn = document.getElementById("next-question");
@@ -233,11 +168,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // function for storing user score
 function calculateUserScore() {
-      // counter for user score
-    let userScore = 0;
-  for (let i = 0; i < questions.choices.length; i++) {
-    userAnswer = questions.choices[i];
-  }
+  // counter for user score
+  let userScore = 0;
+  let userAnswer = storeUserAnswer();
   // if-else statement that compares the user answer to correct choice and adds to user score
   if (userAnswer === quizData.correctChoice) {
     userScore++;

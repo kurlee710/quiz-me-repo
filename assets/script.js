e58205e1cb8689c3e3c2bbbd2d0400425afa98fc
Aditi Rai <aditi45@gmail.com>
2024-09-12 10:58:52 -0400

    created score counter and if statement and store score in local storage

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,9 @@
-// create an empty array that stores correct answers
-const quizAnswers = [];
+// start quiz button to kick off quiz
+// counter for user score
+userScore = 0;
+// if-else statement that compares the user answer to correct choice and adds to user score
+if (userAnswer === correctChoice) {
+  userScore++;
+}
+// store each question score in local storage - json.stringify - part of if-else statement
+localStorage.setItem("userScore", JSON.stringify(userScore));
